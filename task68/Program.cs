642255bfc0b732c83f7af3c85f3ab25f04f767be
Maria <mariya-ivanova@list.ru>
2024-06-23 21:57:46 +0200

    the task68 was solved

---
+++ task68/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Akkerman(int i, int j){
+    if (i == 0)
+    {
+        return j + 1;
+    }
+    else if (j == 0 && i > 0)
+    {
+        return Akkerman(i - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(i - 1, Akkerman(i, j - 1)));
+    }
+}
+
+int GetNumber(string message){
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetNumber("Введите ваше число M: ");
+int n = GetNumber("Введите ваше число N: ");
+Console.Write(Akkerman(m, n));
