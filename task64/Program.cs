31cece44826dafdc4010281ef5ba5f3437a8a0ea
Maria <mariya-ivanova@list.ru>
2024-06-23 20:22:07 +0200

    the task64 was solved

---
+++ task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string WriteIntNumber(int num){
+    if (num == 1){
+        return "1";
+    }
+    else {
+        return num.ToString() + ", " + WriteIntNumber(num-1);
+    }
+}
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = GetNumber("Введите ваше число: ");
+Console.WriteLine($"N = {N} -> [{WriteIntNumber(N)}]");
